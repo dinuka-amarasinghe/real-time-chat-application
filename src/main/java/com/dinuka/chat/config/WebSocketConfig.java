9f219bc6b3bb4fbedb74b998fb40aac8626f035f
dinukah99 <dinukahamarasinghe@gmail.com>
2024-06-22 23:41:21 +0530

    websocket configuration

--- src/main/java/com/dinuka/chat/config/WebSocketConfig.java
+++ src/main/java/com/dinuka/chat/config/WebSocketConfig.java
@@ -13,9 +13,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
     }
 }
