661b2fe230e1ed20cd22cf79f0ca39212a5dd5ce
dinukah99 <dinukahamarasinghe@gmail.com>
2024-06-23 21:27:47 +0530

    broadcast user disconnection messages

--- src/main/java/com/dinuka/chat/config/WebSocketEventListener.java
+++ src/main/java/com/dinuka/chat/config/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package com.dinuka.chat.config;
 
+import com.dinuka.chat.chat.ChatMessage;
+import com.dinuka.chat.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -11,8 +15,20 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messagingTemplate;
+
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-        //TODO
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = headerAccessor.getSessionAttributes().get("username").toString();
+        if (username != null) {
+            log.info("User Disconnected : {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .content("User Disconnected")
+                    .build();
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        }
     }
 }
