5da74e2d90b586008b140489accf74aadc28f2a0
dinukah99 <dinukahamarasinghe@gmail.com>
2024-06-23 21:23:24 +0530

    session attribute handling

--- src/main/java/com/dinuka/chat/chat/ChatController.java
+++ src/main/java/com/dinuka/chat/chat/ChatController.java
@@ -4,6 +4,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 
 @Controller
 public class ChatController {
@@ -11,6 +12,11 @@ public class ChatController {
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
     }
 }
