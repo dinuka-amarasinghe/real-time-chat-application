e4d63d45a180b178aeee9051190d97534e320677
dinukah99 <dinukahamarasinghe@gmail.com>
2024-06-26 23:38:57 +0530

    add message receive and display logic

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,45 +1,37 @@
 'use strict';
 
-var usernamePage = document.querySelector("#username-page");
-var chatPage = document.querySelector("#chat-page");
-var usernameForm = document.querySelector("#usernameForm");
-var messageForm = document.querySelector("#messageForm");
-var messageInput = document.querySelector("#message");
-var messageArea = document.querySelector("#messageArea");
-var connectingElement = document.querySelector(".connecting");
+const usernamePage = document.querySelector("#username-page");
+const chatPage = document.querySelector("#chat-page");
+const usernameForm = document.querySelector("#usernameForm");
+const messageForm = document.querySelector("#messageForm");
+const messageInput = document.querySelector("#message");
+const messageArea = document.querySelector("#messageArea");
+const connectingElement = document.querySelector(".connecting");
 
-var stompClient = null;
-var username = null;
+let stompClient = null;
+let username = null;
 
-var colors = [
+const  colors = [
     '#2196F3', '#32c787', '#00BCD4', '#ff5652',
     '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
 ];
 
-function onError() {
-
-}
-
-function connect(event) {
+function connect(e) {
     username = document.querySelector("#name").value.trim();
 
     if(username) {
         usernamePage.classList.add('hidden');
         chatPage.classList.remove('hidden');
 
-        var socket = new SockJS('/ws');
+        const socket = new SockJS('/ws');
         stompClient = Stomp.over(socket);
 
         stompClient.connect({}, onConnected, onError);
     }
-    event.preventDefault();
+    e.preventDefault();
 }
 
 usernameForm.addEventListener('submit', connect, true);
-
-function onMessageReceived() {
-
-}
 
 function onConnected() {
     stompClient.subscribe('/topic/public', onMessageReceived);
@@ -49,16 +41,52 @@ function onConnected() {
     connectingElement.classList.add('hidden');
 }
 
-function onError(error) {
+function onError() {
     connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
     connectingElement.style.color = 'red';
 }
 
+function onMessageReceived(payload) {
+    const message = JSON.parse(payload.body);
 
-function sendMessage(event) {
-    var messageContent = messageInput.value.trim();
+    const messageElement = document.createElement('li');
+
+    if(message.type === 'JOIN') {
+        messageElement.classList.add('event-message');
+        message.content = message.sender + ' joined!';
+    } else if (message.type === 'LEAVE') {
+        messageElement.classList.add('event-message');
+        message.content = message.sender + ' left!';
+    } else {
+        messageElement.classList.add('chat-message');
+
+        const avatarElement = document.createElement('i');
+        const avatarText = document.createTextNode(message.sender[0]);
+        avatarElement.appendChild(avatarText);
+        avatarElement.style['background-color'] = getAvatarColor(message.sender);
+
+        messageElement.appendChild(avatarElement);
+
+        const usernameElement = document.createElement('span');
+        const usernameText = document.createTextNode(message.sender);
+        usernameElement.appendChild(usernameText);
+        messageElement.appendChild(usernameElement);
+    }
+
+    const textElement = document.createElement('p');
+    const messageText = document.createTextNode(message.content);
+    textElement.appendChild(messageText);
+
+    messageElement.appendChild(textElement);
+
+    messageArea.appendChild(messageElement);
+    messageArea.scrollTop = messageArea.scrollHeight;
+}
+
+function sendMessage(e) {
+    const messageContent = messageInput.value.trim();
     if(messageContent && stompClient) {
-        var chatMessage = {
+        const chatMessage = {
             sender: username,
             content: messageInput.value,
             type: 'CHAT'
@@ -66,5 +94,9 @@ function sendMessage(event) {
         stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
         messageInput.value = '';
     }
-    event.preventDefault();
+    e.preventDefault();
 }
+
+
+usernameForm.addEventListener('submit', connect, true);
+messageForm.addEventListener('submit', sendMessage, true);
